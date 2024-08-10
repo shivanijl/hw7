33ef8b161c6e89d34bdd06a5c30ff8de6690e854
shivanijl <shivanig.jetlearn@gmail.com>
2024-08-10 22:10:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+g1 = ("Red" , "4" , "June 2nd" , "Mumbai" , "Gold")
+
+g2 = ("green" , "5" , "June 5th" , "Pune" , "Silver")
+
+g3 = ("Blue" , "4" , "July 8th" , "Nashik" , "Gold")
+
+g4 = ("yellow" , "5" , "July 15th" , "Goa" , "Silver")
+
+g5 = ("black" , "4" , "August 8th" , "Mumbai" , "Gold")
+
+groupname , sizeofgroup , dateofcompeitition , venue , medaltype = g1
+
+groupname , sizeofgroup , dateofcompeitition , venue , medaltype = g2
+groupname , sizeofgroup , dateofcompeitition , venue , medaltype = g3
+groupname , sizeofgroup , dateofcompeitition , venue , medaltype = g4
+groupname , sizeofgroup , dateofcompeitition , venue , medaltype = g5
+
+print(g1)
+print(g2)
+print(g3)
+print(g4)
+print(g5)
